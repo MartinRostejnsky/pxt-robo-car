27efbc50444b2102aa2bd323844c4a8de1405379
MartinRostejnsky <91248036+MartinRostejnsky@users.noreply.github.com>
2022-06-08 20:45:06 +0200

    no jako nekam se posouvam

--- main.py
+++ main.py
@@ -1,3 +1,78 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+
+#speedFactor = 80
+#pin_L = DigitalPin.P13
+#pin_R = DigitalPin.P14
+#pin_Trig = DigitalPin.P8
+#pin_Echo = DigitalPin.P15
+line_tracking = 0
+speed = 220
+connected = 0
+bluetooth.start_uart_service()
+
+def picovinanefukncni():
+    global line_tracking
+    while line_tracking == 1:
+        print(line_tracking)
+        left = pins.digital_read_pin(DigitalPin.P13)
+        right = pins.digital_read_pin(DigitalPin.P2)
+        pause(400)
+        if left == right:
+            PCAmotor.motor_run(PCAmotor.Motors.M1, 140)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, 140)
+        elif left == 1:
+            PCAmotor.motor_run(PCAmotor.Motors.M1, 140)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, 0)
+        elif right == 1:
+            PCAmotor.motor_run(PCAmotor.Motors.M2, 140)
+            PCAmotor.motor_run(PCAmotor.Motors.M1, 0)
+
+
+def on_bluetooth_connected():
+    global connected, line_tracking
+    basic.show_icon(IconNames.HEART)
+    connected = 1
+    while connected == 1 and line_tracking == 0:
+        uartData = bluetooth.uart_read_until(serial.delimiters(Delimiters.HASH))
+        console.log_value("data", uartData)
+        if uartData == "0":
+            PCAmotor.motor_run(PCAmotor.Motors.M1, 0)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, 0)
+        if uartData == "S":
+            line_tracking = 1
+            picovinanefukncni()
+        if uartData == "A":
+            PCAmotor.motor_run(PCAmotor.Motors.M1, speed*0.85)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, speed)
+        if uartData == "B":
+            PCAmotor.motor_run(PCAmotor.Motors.M1, -(0.85*(speed)))
+            PCAmotor.motor_run(PCAmotor.Motors.M2, -(speed))
+        if uartData == "C":
+            PCAmotor.motor_run(PCAmotor.Motors.M1, 220)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, 0)
+        if uartData == "D":
+            PCAmotor.motor_run(PCAmotor.Motors.M1, 0)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, 220)
+        if uartData == "E":
+            PCAmotor.motor_run(PCAmotor.Motors.M1, 0)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, -220)
+        if uartData == "F":
+            PCAmotor.motor_run(PCAmotor.Motors.M1, -220)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, 0)
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    global connected
+    basic.show_icon(IconNames.SAD)
+    connected = 0
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+# def on_forever():
+#     obstacle_distance = sonar.ping(pin_Trig, pin_Echo, PingUnit.CENTIMETERS, 100)
+
+#     l = False if (whiteline ^ pins.digital_read_pin(pin_L)) == 0 else True
+#     r = False if (whiteline ^ pins.digital_read_pin(pin_R)) == 0 else True
+
+#     # TO DO ...
+
+#     basic.pause(50) #reakční frekvence 20 Hz
+# basic.forever(on_forever)
